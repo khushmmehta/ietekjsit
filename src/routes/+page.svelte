08305c7ff52aa96217e1dd2dd9097ef6b8404b86
Khush Mehta <contact@khushm.com>
2026-02-08 08:49:21 +0530

    feat: removed d-rain, setup basic about in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<script lang="ts">
-	import MatrixRain from '$lib/MatrixRain.svelte';
-</script>
-
-<MatrixRain />
+<article class="flex w-screen flex-col">
+	<div class="self-start p-4 md:w-3xl lg:w-4xl">
+		<h1 class="text-6xl text-green-300 text-shadow-[0px_0px_24px_#00ff00]">What We're About.</h1>
+		<p class="anim-typewriter text-2xl">
+			We are the IETE Student Chapter of KJSIT—a nexus for creators, thinkers, and pioneers. We
+			transcend the conventional, pushing the boundaries of technology and engineering to build a
+			smarter tomorrow. This is our digital hub.
+		</p>
+	</div>
+</article>
