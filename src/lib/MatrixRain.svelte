5cc09c129e2559cb5d6697aa5d896e8a23409d55
Khush Mehta <contact@khushm.com>
2026-02-09 12:49:35 +0530

    feat: remove typewriter anims, add base code for matrix rain

---
+++ src/lib/MatrixRain.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Color } from "three";
+
+	let canvas: HTMLCanvasElement;
+
+	// New Matrix Rain Algorithm:
+	//   Length & colors determined on initialization.
+	//   Gap randomly generated thereafter.
+	//   Next line ensues.
+	//   Each character cell is a Symbol class which randomly shifts in letters.
+
+	// TODO:
+	//   Figure out how to do automated text masking.
+	//   Create a glow effect based on cursor position.
+	//   Layering of the digital rain for a more cinematic feel.
+
+	class Symbol {
+		characters: String;
+		x: number;
+		y: number;
+		color: Color;
+		fontSize: number;
+
+		constructor(x, y, fontSize, color) {
+			this.characters =
+				'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+				'0123456789!@#$%^&*()[]{}?><' +
+				'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+			this.x = x;
+			this.y = y;
+			this.fontSize = fontSize;
+			this.color = color;
+		}
+	}
+
+	$effect(() => {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+
+		const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
+		window.addEventListener('resize', function () {
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+		});
+	});
+</script>
+
+<canvas bind:this={canvas}></canvas>
