71518d9f89d9905c5543e03f60b7e85187cb447d
LinuxGuy312 <githubwh@gmail.com>
2026-02-16 00:57:56 +0530

    fix: remove corner decorations and redundant code

--- src/lib/components/OscillationHero.svelte
+++ src/lib/components/OscillationHero.svelte
@@ -99,12 +99,6 @@
 					</div>
 				</div>
 			</div>
-
-			<!-- Decorative Corners -->
-			<div class="absolute top-0 left-0 h-2 w-2 border-t-2 border-l-2 border-green-500"></div>
-			<div class="absolute top-0 right-0 h-2 w-2 border-t-2 border-r-2 border-green-500"></div>
-			<div class="absolute bottom-0 left-0 h-2 w-2 border-b-2 border-l-2 border-green-500"></div>
-			<div class="absolute right-0 bottom-0 h-2 w-2 border-r-2 border-b-2 border-green-500"></div>
 		</div>
 	</div>
 </section>
